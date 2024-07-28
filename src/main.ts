a126adde9750017daacf3ea1fb8a7669b9d31085
Valter Junior <valter.junior@dieboldnixdorf.com>
2024-07-28 11:28:33 -0300

    ok

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { provideRouter } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { PoModule } from '@po-ui/ng-components';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { APP_ROUTES } from './app/app.routes';
